a6794be19e6f923ed06fe0e98f414b3324522d34
newgirll <k.kowalska2407@gmail.com>
2018-03-18 18:00:31 +0100

    add images

--- Recruitment_task_H4/public/scripts/app.js
+++ Recruitment_task_H4/public/scripts/app.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
     const eye = document.querySelectorAll(".box__list__item--eye");
     const weight = document.querySelectorAll(".box__list__item--weight");
-    const list = document.querySelector(".box__list");
-    const arrow = document.querySelectorAll(".arrow");
+    // const list = document.querySelector(".box__list");
+    // const arrow = document.querySelectorAll(".arrow");
 
 
     for(var i = 0; i < eye.length; i++) {
         if(typeof eye[i] !== "undefined"){
             eye[i].addEventListener("mouseover", function(){
-                this.setAttribute("src", "./src/images/okohover.png");
+                this.setAttribute("src", "./public/images/okohover.png");
             })
             eye[i].addEventListener("mouseleave", function(){
-                this.setAttribute("src", "./src/images/oko.png");
+                this.setAttribute("src", "./public/images/oko.png");
             })
         }
     }
@@ -19,18 +19,18 @@ document.addEventListener("DOMContentLoaded", function(){
     for(var i = 0; i < weight.length; i++) {
         if(typeof weight[i] !== "undefined"){
             weight[i].addEventListener("mouseover", function(){
-                this.setAttribute("src", "./src/images/wagahover.png");
+                this.setAttribute("src", "./public/images/wagahover.png");
             })
             weight[i].addEventListener("mouseleave", function(){
-                this.setAttribute("src", "./src/images/waga.png");
+                this.setAttribute("src", "./public/images/waga.png");
             })
         }
     }
 
-    for(var i = 0; i< arrow.length; i++){
-        arrow[i].addEventListener("touchenter", function(){
-            console.log("tak")
-        })
-    }
+    // for(var i = 0; i< arrow.length; i++){
+    //     arrow[i].addEventListener("touchenter", function(){
+    //         console.log("tak")
+    //     })
+    // }
     
 })
